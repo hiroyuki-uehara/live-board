00e562f63672cfe981b0f8aeabc870aa06533fbb
hiroyuki-uehara <n_c_par7@yahoo.co.jp>
2021-04-08 01:16:13 +0900

    initialize Live Board

---
+++ src/assets/icons/FontAwesome.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <font-awesome-icon :icon="['fas', 'reply']" />
+    <font-awesome-icon :icon="['far', 'edit']" />
+    <font-awesome-icon :icon="['fab', 'github']" />
+    <font-awesome-icon :icon="['far', 'bell']" />
+    <font-awesome-icon :icon="['fal', 'trash-alt']" />
+    <font-awesome-icon :icon="['fad', 'coffee']" />
+    <v-gravatar email="jay@longisland.com" :size="30" default-img="identicon" />
+    <v-gravatar email="jay@longisland.com" :size="30" default-img="mp" />
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,67 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store/store';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChalkboard, faCircle, faReply } from '@fortawesome/free-solid-svg-icons';
+import { faWindowClose, faEdit } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBell, faPlusCircle } from '@fortawesome/pro-regular-svg-icons';
+import { faTrashAlt, faQuestionCircle } from '@fortawesome/pro-light-svg-icons';
+import { faCoffee } from '@fortawesome/pro-duotone-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import Gravatar from 'vue-gravatar';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUHjJmnJLPkwrScGiAY3luHewFDj6bldA',
+  authDomain: 'live-board-1868b.firebaseapp.com',
+  projectId: 'live-board-1868b',
+  storageBucket: 'live-board-1868b.appspot.com',
+  messagingSenderId: '2037367130',
+  appId: '1:2037367130:web:6db35f07da52cf17e67363',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+library.add(
+  faChalkboard,
+  faCircle,
+  faReply,
+  faWindowClose,
+  faEdit,
+  faGithub,
+  faBell,
+  faPlusCircle,
+  faTrashAlt,
+  faQuestionCircle,
+  faCoffee
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('v-gravatar', Gravatar);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
