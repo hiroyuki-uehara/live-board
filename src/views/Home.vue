2af00f6873325b2f4562cc4ce794daf56046e49d
hiroyuki-uehara <n_c_par7@yahoo.co.jp>
2021-04-08 21:07:17 +0900

    modify _background.scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Header>
+      <router-link to="/board" class="mt-3 ml-auto mr-3">
+        <b-button variant="outline-success"><p>to Board</p></b-button>
+      </router-link>
+    </Header>
+    <div id="main"></div>
+    <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {};
+  },
   components: {
-    HelloWorld,
+    Header,
+    Footer,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/sass/reset';
+@import '../assets/sass/background';
+@import '../assets/sass/home';
+</style>
